a0008e8d1d35d98b526c5d752e93706cb9378723
PavanKumar <pavankumar.ambraji@gmail.com>
2023-09-17 18:13:30 +0530

    Spring Boot Runners Example initial commit

---
+++ src/main/java/com/runners/MyCommandLineRunner.java
@@ -0,0 +1,17 @@
+package com.runners;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyCommandLineRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Command-line arguments for CommandLineRunner:");
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+	}
+
+}
